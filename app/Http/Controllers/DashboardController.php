fa6f1196b5d988d8922026b2d868c60408848b3b
Julio Cesar MBP <juliocesar@sediweb.com>
2018-04-21 00:59:04 -0400

    arreglos en el UI en dashboard para el error cuando no hay ciclo actual

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,8 +16,8 @@ class DashboardController extends Controller
     	$ciclos = Ciclo::all();
     	$aprobadas = Asignatura::all()->where('aprovado',1);
     	$pendientes = Asignatura::all()->where('aprovado',0);
-    	$id_cicloActual = Ciclo::where('cerrado',0)->first()->id;
-    	$actuales= Grupo::where('id_ciclo',$id_cicloActual)->get(); 
+    	$id_cicloActual = Ciclo::where('cerrado',0)->first();
+    	if($id_cicloActual) {$actuales= Grupo::where('id_ciclo',$id_cicloActual->id)->get();} else{$actuales=0;} 
     	return view('dashboard',compact('asignaturas','ciclos','aprobadas','pendientes','actuales'));
 
     }
