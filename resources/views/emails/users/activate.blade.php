f9de096ad639ab32c840fd16f2634d985ea3e5a4
Julio Cesar Veras <juliocesar@sediweb.com>
2018-09-01 12:04:21 -0400

    arreglar el correo que se envia al user para activar la cuenta

--- resources/views/emails/users/activate.blade.php
+++ resources/views/emails/users/activate.blade.php
@@ -2,9 +2,15 @@
 # Bienvenido {{ $user->full_name }}
 
 The body of your message.
+{{ "Tu codigo de activacion es: ". $user->activate_code  }}
 
-@component('mail::button', ['url' => route('profiles')])
+@component('mail::button', ['url' => route('login.activate', $user->salt)])
 Activar Cuenta
+@endcomponent
+
+
+@component('mail::header', ['url' => route('login.activate', $user->salt)])
+{{ config('app.name') }}
 @endcomponent
 
 Thanks,<br>
