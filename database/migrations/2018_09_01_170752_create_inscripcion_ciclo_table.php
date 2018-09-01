37ac4483ea3a22adb605e769729799fa62503c03
Julio Cesar Veras <juliocesar@sediweb.com>
2018-09-01 14:22:11 -0400

    Se borraron las siguientes tablas de la base de datos:  calificaciones y facilitadores, tambien se borraron los modelos facilitadores y calificaciones. se eliminaron las columnas de facilitadores_id  y se anadio la columna seccion de la table de grupos.

---
+++ database/migrations/2018_09_01_170752_create_inscripcion_ciclo_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInscripcionCicloTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inscripcion_ciclo', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('nota')->nullable();
+            $table->string('clave_ciclo');
+            
+            $table->unsignedInteger('grupo_id');
+            $table->foreign('grupo_id')->references('id')->on('grupos');
+            
+            $table->unsignedInteger('usuario_id');
+            $table->foreign('usuario_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inscripcion_ciclo');
+    }
+}
