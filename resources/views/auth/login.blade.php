58a7809301a8e89093eae171ad465568e4273d0e
Julio Cesar Veras <juliocesar@sediweb.com>
2018-09-10 00:16:14 -0400

    Modificar UI pantalla de inicio y login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,116 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!--Intro Section-->
+            <section class="view intro-2 hm-gradient">
+                <div class="full-bg-img">
+                    <div class="container flex-center">
+                        <div class="d-flex align-items-center content-height">
+                            <div class="row flex-center">
+                                <div class="col-md-6 text-center text-md-left mb-5">
+                                    <div class="white-text">
+                                        <h1 class="h1-responsive font-weight-bold wow fadeInLeft" data-wow-delay="0.3s">Sistema de Control de Asignaturas </h1>
+                                        <hr class="hr-light wow fadeInLeft" data-wow-delay="0.3s">
+                                        <h6 class="wow fadeInLeft" data-wow-delay="0.3s">
+                                                Este sistema le brinda la información que necesita acerca de su estado académico en la UAPA. 
+                                                Nuestro sistema se alimenta de la información que aparece en la sistema académico de la Universidad por lo cual no nos hacemos responsables de veracidad de la información que se brinda. 
+                                                Las informaciones oficiales y finales siempre serán las que brinda el departamento de Registro de la universidad.
+                                                Advertencia: Su matrícula y contraseña serán almacenadas en nuestra base de datos, las cuales
+                                                serán utilizadas por el sistema para acceder periodicamente a la plataforma de UAPA.
+                                        </h6>
+                                        <br>
+                                        <a class="btn btn-outline-white wow fadeInLeft" data-wow-delay="0.3s">Learn more</a>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6 col-xl-5 offset-xl-1">
+                                    <!--Form-->
+                                    <div class="card wow fadeInRight" data-wow-delay="0.3s">
+                                        <div class="card-body">
+                                                <form method="POST" action="{{ route('login') }}">
+                                                        @csrf
+                                            <!--Header-->
+                                            <div class="text-center">
+                                                <h3 class="white-text"><i class="fa fa-user white-text"></i> Registrarse</h3>
+                                                <hr class="hr-light">
+                                            </div>
+
+                                            <!--Body-->
+                                            <div class="md-form">
+                                                    <i class="fa fa-user prefix white-text active"></i>
+                                                        <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}"
+                                                            name="username" value="{{ old('username') }}" required>
+                                                        @if ($errors->has('username'))
+                                                        <span class="invalid-feedback">
+                                                            <strong>{{ $errors->first('username') }}</strong>
+                                                        </span>
+                                                        @endif
+                                                        <label for="username" class="white-text active">{{ __('Matrícula')}}</label>                                              
+                                                </div>
+
+                                                <div class="md-form">
+                                                        <i class="fa fa-lock prefix white-text active"></i>
+                                                        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                                                        name="password" required>
+                
+                                                    @if ($errors->has('password'))
+                                                    <span class="invalid-feedback">
+                                                        <strong>{{ $errors->first('password') }}</strong>
+                                                    </span>
+                                                    @endif
+                                                    <label for="password" class="white-text active">{{ __('Contraseña')}}</label>                             
+                                                </div>
+
+                            <div class="form-group row">
+                                <div class="col-md-6 offset-md-4">
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" name="remember"
+                                                {{ old('remember') ? 'checked' : '' }}>
+                                            {{ __('Recordar mi usuario') }}
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-indigo">
+                                        {{ __('Entrar') }}
+                                    </button>                                    
+                                </div>
+                            </div>
+                            
+                            <div class="text-center">
+                                <hr class="hr-light mb-3 mt-4">
+
+                                <div class="inline-ul text-center d-flex justify-content-center">
+                                    <a class="" href="{{ route('password.request') }}">
+                                        {{ __('Olvidé mi contraseña') }}
+                                    </a>
+                                    {{--  <a class="icons-sm tw-ic"><i class="fa fa-twitter white-text"></i></a>
+                                    <a class="icons-sm li-ic"><i class="fa fa-linkedin white-text"> </i></a>
+                                    <a class="icons-sm ins-ic"><i class="fa fa-instagram white-text"> </i></a>  --}}
+                                </div>
+                            </div>
+                        </form>
+                                       </div>
+                                    </div>
+                                    <!--/.Form-->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </header>
+        <!--Main Navigation-->
+
+
+
+
+{{--  <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -79,6 +188,6 @@
             </div>
         </div>
     </div>
-</div>
+</div>  --}}
 
 @endsection
